966486324290c56da27147c39a4c66c8a8a04799
Oceaan Two <oceaantwo@Oceaans-MacBook-Air.local>
2025-01-11 12:09:53 -0800

    create function for writing to client and use for html

--- src/main.c
+++ src/main.c
@@ -11,9 +11,9 @@
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
-#define HTML_LENGTH 100
 #define HTTP_OK "HTTP/1.0 200 OK\r\n"
 #define HTTP_CONTENT_TYPE "Content-Type: text/html\r\n"
+#define HTTP_CONTENT_SIZE 22
 
 // Priorities:
 //  Serve different file types
@@ -22,10 +22,11 @@
 //  Handle GET and HEAD
 //  Implement multiplexing or threads
 
+int write_to_client(int newsockfd, const char *ok_msg, const char *content_type, const char *content_length, unsigned long content_length_size);
+
 int main(int arg, const char *argv[])
 {
     char               buffer[BUFFER_SIZE];
-    char               response_string[HTML_LENGTH];
     struct sockaddr_in host_addr;
     unsigned int       host_addrlen;
 
@@ -72,13 +73,9 @@ int main(int arg, const char *argv[])
 
     while(1)
     {
-        int           sockn;
-        ssize_t       valread;
-        ssize_t       valwrite;
-        int           fd;
-        char          c;
-        char          html_string[BUFFER_SIZE];
-        unsigned long length = 0;
+        int     sockn;
+        ssize_t valread;
+        ssize_t valwrite;
 
         // Accept incoming connections
         int newsockfd = accept(sockfd, (struct sockaddr *)&host_addr, (socklen_t *)&host_addrlen);
@@ -107,39 +104,13 @@ int main(int arg, const char *argv[])
         printf("[%s:%u]\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
         printf("%s\n", buffer);
 
-        // if valread is a GET request for html
-        // serve the html file
-        strcpy(response_string, HTTP_OK);
-        strncat(response_string, HTTP_CONTENT_TYPE, strlen(HTTP_CONTENT_TYPE));
-        // 25 is the length of the hello world html file
-        strncat(response_string, "Content-Length: 25\r\n\r\n", 22);
-        // get the content of the file index.html with fd and put it in resp
-        fd = open("./resources/index.html", O_RDONLY | O_CLOEXEC);
-        if(fd == -1)
+        // TODO: if or switch statements for different kinds of acceptable requests and what they should return
+        valwrite = write_to_client(newsockfd, HTTP_OK, HTTP_CONTENT_TYPE, "Content-Length: 25\r\n\r\n", HTTP_CONTENT_SIZE);
+        if(valwrite == -1)
         {
-            perror("webserver (open)");
             continue;
         }
-        printf("reading from html file\n");
-        while((valread = read(fd, &c, 1)) > 0 && c != '\0')
-        {
-            // write c to html_string
-            html_string[length++] = c;
-        }
-        if(valread < 0)
-        {
-            perror("webserver (read)");
-            continue;
-        }
-        html_string[length] = '\0';
-        strncat(response_string, html_string, length);
-        strncat(response_string, "\r\n", 2);
-        valwrite = write(newsockfd, response_string, strlen(response_string));
-        if(valwrite < 0)
-        {
-            perror("webserver (write)");
-            continue;
-        }
+
         printf("closing connection\n");
         close(newsockfd);
     }
@@ -148,4 +119,50 @@ int main(int arg, const char *argv[])
 #pragma GCC diagnostic ignored "-Wunreachable-code-return"
     return 0;
 #pragma GCC diagnostic pop
+}
+
+int write_to_client(int newsockfd, const char *ok_msg, const char *content_type, const char *content_length, unsigned long content_length_size)
+{
+    char          response_string[BUFFER_SIZE];
+    char          content_string[BUFFER_SIZE];
+    int           file_fd;
+    ssize_t       valread;
+    ssize_t       valwrite;
+    char          c;
+    unsigned long length = 0;
+
+    // if valread is a GET request for html
+    // serve the html file
+    strncpy(response_string, ok_msg, strlen(ok_msg));
+    strncat(response_string, content_type, strlen(content_type));
+    // 25 is the length of the hello world html file
+    strncat(response_string, content_length, content_length_size);
+    // get the content of the file index.html with fd and put it in resp
+    file_fd = open("./resources/index.html", O_RDONLY | O_CLOEXEC);
+    if(file_fd == -1)
+    {
+        perror("webserver (open)");
+        return -1;
+    }
+    printf("reading from html file\n");
+    while((valread = read(file_fd, &c, 1)) > 0 && c != '\0')
+    {
+        // write c to response_string
+        content_string[length++] = c;
+    }
+    if(valread < 0)
+    {
+        perror("webserver (read)");
+        return -1;
+    }
+    content_string[length] = '\0';
+    strncat(response_string, content_string, length);
+    strncat(response_string, "\r\n", 2);
+    valwrite = write(newsockfd, response_string, strlen(response_string));
+    if(valwrite < 0)
+    {
+        perror("webserver (write)");
+        return -1;
+    }
+    return 0;
 }
