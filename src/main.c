ad0a867f752ce7881640f16d1c321636133151e4
Oceaan Two <oceaantwo@Oceaans-MacBook-Air.local>
2025-01-12 14:35:02 -0800

    add 404 not found response

--- src/main.c
+++ src/main.c
@@ -12,19 +12,24 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 #define HTTP_OK "HTTP/1.0 200 OK\r\n"
+#define HTTP_NOT_FOUND "HTTP/1.0 404 Not Found\r\n"
 #define HTTP_CONTENT_TYPE "Content-Type: text/html\r\n"
-#define HTTP_CONTENT_SIZE 22
+#define HTTP_OK_CONTENT_LENGTH_MSG "Content-Length: 25\r\n\r\n"
+#define HTTP_404_CONTENT_LENGTH_MSG "Content-Length: 20\r\n\r\n"
 #define REQ_HEADER_LEN 5
 #define PATH_LEN 1024
 
+// FILE_PATH_LEN is the length we have to append to include ./resources to the path
+// we want to open the file we're serving at
+#define FILE_PATH_LEN 11
+
 // Priorities:
 //  Serve different file types
-//  Construct response correctly (make a struct?)
 //  Handle incorrect requests
 //  Handle GET and HEAD
 //  Implement multiplexing or threads
 
-int  write_to_client(int newsockfd, const char *ok_msg, const char *content_type, const char *content_length, unsigned long content_length_size);
+int  write_to_client(int newsockfd, const char *request_path);
 int  is_get_request(const char *req_header);
 int  is_head_request(const char *req_header);
 void set_request_path(char *req_path, const char *buffer);
@@ -125,7 +130,7 @@ int main(int arg, const char *argv[])
         set_request_path(req_path, buffer);
 
         // TODO: if or switch statements for different kinds of acceptable requests and what they should return
-        valwrite = write_to_client(newsockfd, HTTP_OK, HTTP_CONTENT_TYPE, "Content-Length: 25\r\n\r\n", HTTP_CONTENT_SIZE);
+        valwrite = write_to_client(newsockfd, req_path);
         if(valwrite == -1)
         {
             continue;
@@ -148,7 +153,7 @@ int main(int arg, const char *argv[])
 #pragma GCC diagnostic pop
 }
 
-int write_to_client(int newsockfd, const char *ok_msg, const char *content_type, const char *content_length, unsigned long content_length_size)
+int write_to_client(int newsockfd, const char *request_path)
 {
     char          response_string[BUFFER_SIZE];
     char          content_string[BUFFER_SIZE];
@@ -158,20 +163,40 @@ int write_to_client(int newsockfd, const char *ok_msg, const char *content_type,
     char          c;
     unsigned long length = 0;
 
-    // if valread is a GET request for html
-    // serve the html file
-    strncpy(response_string, ok_msg, strlen(ok_msg));
-    strncat(response_string, content_type, strlen(content_type));
-    // 25 is the length of the hello world html file
-    strncat(response_string, content_length, content_length_size);
-    // get the content of the file index.html with fd and put it in resp
-    file_fd = open("./resources/index.html", O_RDONLY | O_CLOEXEC);
+    // get the content of the file with fd and put it in resp
+    if(strcmp(request_path, "/") == 0)
+    {
+        file_fd = open("./resources/index.html", O_RDONLY | O_CLOEXEC);
+    }
+    else
+    {
+        char *path = (char *)malloc(sizeof(char) * (strlen(request_path) + FILE_PATH_LEN));
+        strncpy(path, "./resources", FILE_PATH_LEN);
+        strncpy(path + FILE_PATH_LEN, request_path, strlen(request_path));
+        printf("file path: %s\n", path);
+        file_fd = open(path, O_RDONLY | O_CLOEXEC);
+        free(path);
+    }
     if(file_fd == -1)
     {
+        // This means the file was not found/openable
         perror("webserver (open)");
-        return -1;
+        strncpy(response_string, HTTP_NOT_FOUND, sizeof(HTTP_NOT_FOUND) - 1);
+        response_string[sizeof(HTTP_NOT_FOUND) - 1] = '\0';
+        file_fd                                     = open("./resources/404.html", O_RDONLY | O_CLOEXEC);
+        if(file_fd == -1)
+        {
+            perror("webserver (open)");
+            return -1;
+        }
     }
-    printf("reading from html file\n");
+    else
+    {
+        strncpy(response_string, HTTP_OK, sizeof(HTTP_OK) - 1);
+        response_string[sizeof(HTTP_OK) - 1] = '\0';
+    }
+
+    printf("reading from file\n");
     while((valread = read(file_fd, &c, 1)) > 0 && c != '\0')
     {
         // write c to response_string
@@ -184,8 +209,25 @@ int write_to_client(int newsockfd, const char *ok_msg, const char *content_type,
         return -1;
     }
     content_string[length] = '\0';
+
+    // only deals with http content type right now
+    strncat(response_string, HTTP_CONTENT_TYPE, strlen(HTTP_CONTENT_TYPE));
+
+    // append content length section
+    if(strcmp(request_path, "/") == 0)
+    {
+        strncat(response_string, HTTP_OK_CONTENT_LENGTH_MSG, length + 2);
+    }
+    else
+    {
+        strncat(response_string, HTTP_404_CONTENT_LENGTH_MSG, length + 2);
+    }
+
+    // append body section (TODO: don't do this if it's a HEAD req)
     strncat(response_string, content_string, length);
     strncat(response_string, "\r\n", 2);
+
+    // write to client
     valwrite = write(newsockfd, response_string, strlen(response_string));
     if(valwrite < 0)
     {
