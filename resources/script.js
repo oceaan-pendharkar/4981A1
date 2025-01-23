4e43af6f37bea602e4664913d1e9b7667a41dfbf
justinsaint <jsaintdev@gmail.com>
2025-01-22 19:59:26 -0800

    Updated HTML file to include png

--- resources/script.js
+++ resources/script.js
@@ -7,13 +7,12 @@ button.addEventListener('click', () => {
     // Clear the current content of the image container
     imageContainer.innerHTML = '';
 
-    // Create three images
     for (let i = 0; i < 3; i++) {
         const img = document.createElement('img');
         img.src = 'dancecat.gif';
         img.alt = 'Dancing Cat';
         img.style.margin = '10px';
-        img.style.width = '200px'; // Optional: Adjust size
+        img.style.width = '200px';
         imageContainer.appendChild(img);
     }
 
